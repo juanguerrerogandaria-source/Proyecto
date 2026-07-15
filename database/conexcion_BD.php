3611b30c44aba9d358b1dcb7c01ed1870d6861f1
Juani Guerrero <juanguerrerogandaria@gmail.com>
2026-07-15 16:41:33 -0300

    conectar al sevidor

--- database/conexcion_BD.php
+++ database/conexcion_BD.php
@@ -1,13 +1,14 @@
 <?php
 
-    $conexion = mysqli_connect("localhost", "root", "", "login_re_proyecto");
+$conexion = mysqli_connect(
+    "sql304.infinityfree.com",
+    "if0_42417739",
+    "x0n6Vzjk0WCj",
+    "if0_42417739_proyecto_web"
+);
 
-    /*
-    if ($conexion) {
-        echo "conexion exsitosa";
-    }
-    else {
-        echo "no funciono";
-    } 
-    */
+if (!$conexion) {
+    die("Error de conexión: " . mysqli_connect_error());
+}
+
 ?>
